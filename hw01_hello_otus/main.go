32bcc167bef6e9c0cca21823cd70b42cddaf54c1
danver <podzyuban@gmail.com>
2021-06-26 01:12:30 +0300

    hw1

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	var input = "Hello, OTUS!"
+	var output = stringutil.Reverse(input)
+	fmt.Println(output)
 }
